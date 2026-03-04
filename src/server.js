eedc57ab44d2bbce6a2d4afc34d764df4420ea1d
natontest <shoppevideo7@gmail.com>
2026-03-04 00:38:52 -0300

    Change static file directory name to 'público'

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../público')))
 
 function isShopeeUrl(url) {
   try {
